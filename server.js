07b3cf3bde28a7631d9da96cdbbe98f8b5505609
takuworks123 <takuworks123@gmail.com>
2022-11-04 13:18:42 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-import { serve } from "https://deno.land/std@0.138.0/http/server.ts"
-console.log("Listening on http://localhost:8000");
+import { serve } from "https://deno.land/std@0.138.0/http/server.ts";
 
-serve(req => {
-    return new Response("Hello World!!!!!!!!!!!!!");
+console.log("Listening on http://localhost:8000");
+serve(async (req) => {
+  const pathname = new URL(req.url).pathname;
+  console.log(pathname);
+
+  if (pathname === "/styles.css") {
+    return new Response(await Deno.readTextFile("./public/styles.css"), {
+      headers: { "Content-Type": "text/css; charset=utf-8" },
+    });
+  }
+
+  return new Response(await Deno.readTextFile("./public/index.html"), {
+    headers: { "Content-Type": "text/html; charset=utf-8" },
+  });
 });
