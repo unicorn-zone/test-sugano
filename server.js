086d96c69bde443ad8b8635ff64d5c333a63fa68
takuworks123 <takuworks123@gmail.com>
2022-11-04 13:15:09 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import { serve } from "https://deno.land/std@0.138.0/http/server.ts"
 console.log("Listening on http://localhost:8000");
 
 serve(req => {
-    return new Response("Hello World");
+    return new Response("Hello World!!!!!!!!!!!!!");
 });
