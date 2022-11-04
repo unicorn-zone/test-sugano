e42dd813315033e3a2b6d215ca9a55fa16af0c4f
takuworks123 <takuworks123@gmail.com>
2022-11-04 13:00:14 +0900

    init

---
+++ server.js
@@ -0,0 +1,6 @@
+import { serve } from "https://deno.land/std@0.138.0/http/server.ts"
+console.log("Listening on http://localhost:8000");
+
+serve(req => {
+    return new Response("Hello World");
+});
