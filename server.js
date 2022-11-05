52157826616b501ba193ac3d810d4bf45b2bfc4d
takuworks123 <takuworks123@gmail.com>
2022-11-06 00:04:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ serve(async (req) => {
   if (req.method === "POST" && pathname === "/code_info2") {
     const requestJson = await req.json();
     let group = requestJson.group;
-    obj = await supabase.from('calendar').select().eq('group', group).rangeGte('date_start', '[2022-11-01 00:00:00, 2022-11-01 00:00:00)');
+    obj = await supabase.from('calendar').select().eq('group', group).rangeGte('date_start', '[2022-11-01 00:00:00, 2022-12-01 00:00:00)');
     if (obj.error == null) {
       let data = obj.data[0].created_at + '||';
       data = data + obj.data[0].date_start + '||';
