f2335adfe100f234cdc659ae1a9c521e991b9110
takuworks123 <takuworks123@gmail.com>
2022-11-06 21:14:28 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@1.34.0';
 
 let url = 'https://ekzwclcfheomwmnteywk.supabase.co';
@@ -122,14 +123,11 @@ serve(async (req) => {
 
 
 
-  if (pathname === "/styles.css") {
-    return new Response(await Deno.readTextFile("./public/styles.css"), {
-      headers: { "Content-Type": "text/css; charset=utf-8" },
-    });
-  }
 
-  return new Response(await Deno.readTextFile("./public/index.html"), {
-    headers: { "Content-Type": "text/html; charset=utf-8" },
+  return serveDir(req, {
+    fsRoot: "public",
+    urlRoot: "",
+    showDirListing: true,
+    enableCors: true,
   });
-
 });
