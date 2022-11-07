c24547ae0aa4badaabd5045a137c3b9735b902d9
takuworks123 <takuworks123@gmail.com>
2022-11-07 12:01:11 +0900

    a

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ serve(async (req) => {
       .select()
       .eq( 'username', requestJson.username )
       .eq( 'password', requestJson.password );
-    return new Response({ status: 999 });
+    return new Response({ status: 999, statusText: 'TEST RES' });
     
     if (sp.error == null) { // エラーがないとき
       if (sp.data.length == 1){ // データベースに、対応するアカウントが１つある場合
